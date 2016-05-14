195937f9404ec4f50d30eb0e6e8eccdeb4cb82f8
ajhausdorf <ajhausdorf@wustl.edu>
2016-05-13 20:29:44 -0500

    initialize

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Module
+var myApp = angular.module('myApp', []);
+
+
+
+/* jQuery w/ noConflict for use in Angular
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+	console.log("ANDREW ");
+}); */
